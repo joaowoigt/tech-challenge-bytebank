354599dc9d0238327ccfb78b8bcda926e47ad6c0
joaowoigt <joaowoigt@gmail.com>
2024-10-12 13:07:42 -0300

    feat: creating transaction detail layout

--- pages/transactionDetails.tsx
+++ pages/transactionDetails.tsx
@@ -1,11 +1,82 @@
 import { useRouter } from "next/router";
+import Text from "@/components/Texts/texts";
+import { useState } from "react";
+import Dropdown, { DropDownItem } from "@/components/DropDown/dropDown";
+import { TransactionType } from "@/models/TransactionType";
+import { CloseIcon, DeleteIcon, EditIcon } from "@/components/Icons/icons";
+import CurrencyInput from "react-currency-input-field";
 
 export default function TransactionDetails() {
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const menuDropDownItems: DropDownItem[] = [
+    { title: "Cambio de Moeda", type: TransactionType.CambioDeMoeda },
+    { title: "DOC/TED", type: TransactionType.DOC },
+    {
+      title: "Empréstimo e Financiamento",
+      type: TransactionType.Emprestimo,
+    },
+    {
+      title: "Salário",
+      type: TransactionType.Salario,
+    },
+  ];
+  const handleEditing = (isEditing: boolean) => {
+    setIsEditing(isEditing);
+  };
+
   const router = useRouter();
   const id = router.query.id;
+  const title = `Detalhes da transação: ${id}`;
   return (
-    <div>
-      <h1> {id}</h1>
-    </div>
+    <section className="bg-secondaryVariant h-[420px] rounded-2xl flex flex-col m-big justify-around p-big">
+      <div className="flex flex-row justify-between">
+        <Text intent="Heading" color="black" text={title}></Text>
+        <div className="flex flex-row">
+          {isEditing ? (
+            <CloseIcon onClick={() => handleEditing(false)}></CloseIcon>
+          ) : (
+            <EditIcon onClick={() => handleEditing(true)}></EditIcon>
+          )}
+          <DeleteIcon onClick={() => {}}></DeleteIcon>
+        </div>
+      </div>
+      <div className="flex flex-col align-middle py-small">
+        <Text intent="Small" color="black" text="Tipo:"></Text>
+        {isEditing ? (
+          <Dropdown
+            items={menuDropDownItems}
+            onSelect={(item: TransactionType) => {}}
+          ></Dropdown>
+        ) : (
+          <Text intent="Regular" color="black" text="DOC/TED"></Text>
+        )}
+      </div>
+      <div className="flex flex-col align-middle py-small">
+        <Text intent="Small" color="black" text="Valor:"></Text>
+
+        {isEditing ? (
+          <CurrencyInput
+            decimalsLimit={2}
+            onValueChange={(value, name, values) => {}}
+            prefix="R$"
+            className="outline outline-1 outline-primary  mb-big mt-medium bg-white rounded-md px-small w-[250px]  py-small text-black text-start flex flex-row hover:cursor-text"
+          />
+        ) : (
+          <Text intent="Regular" color="black" text="R$ 12,59"></Text>
+        )}
+      </div>
+      <div className="flex flex-col align-middle py-small">
+        <Text intent="Small" color="black" text="Data:"></Text>
+        {isEditing ? (
+          <input
+            onChange={(date) => console.log(date.target.value)}
+            type="date"
+            className="outline outline-1 outline-primary  mb-big mt-medium bg-white rounded-md px-small w-[250px]  py-small text-black text-start flex flex-row hover:cursor-text"
+          />
+        ) : (
+          <Text intent="Regular" color="black" text="12/10/23"></Text>
+        )}
+      </div>
+    </section>
   );
 }
